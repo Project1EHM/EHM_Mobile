d9899c5ff3d7168400f787c9cbf938848f315535
sunny2jk <sunny2jk@gmail.com>
2016-05-02 16:39:38 +0700

    Commit Project

---
+++ www/js/app.js
@@ -0,0 +1,282 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.services' is found in services.js
+// 'starter.controllers' is found in controllers.js
+
+angular.module('ionic.utils', [])
+
+.factory('$localstorage', ['$window', function($window) {
+  return {
+    set: function(key, value) {
+      window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse(window.localStorage[key] || '{}');
+    },
+    clear: function() {
+      window.localStorage.clear();
+    }
+  }
+}]);
+
+angular.module('starter', ['ionic','ionic.service.core', 'starter.controllers', 'starter.services', 'ionic.utils','ui.rCalendar',])
+
+.run(function($ionicPlatform,$localstorage,$ionicPopup) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+    var pushNotification = window.plugins.pushNotification;
+    
+
+    window.successHandler = function(result) {
+      //alert('Callback Success! Result = '+result)
+    }
+
+    window.onNotification = function(e){
+
+      switch( e.event )
+      {
+        case 'registered':
+        if ( e.regid.length > 0 )
+        {
+          console.log("Regid " + e.regid);
+          $localstorage.setObject('push', {
+            token: e.regid,
+            });
+        }
+        break;
+
+        case 'message':
+              var alertPopup = $ionicPopup.alert({
+        title: 'ข้อความ!',
+        template: e.message
+      });
+              break;
+
+              case 'error':
+              alert('GCM error = '+e.msg);
+              break;
+
+              default:
+              alert('An unknown GCM event has occurred');
+              break;
+            }
+       }
+
+          window.errorHandler = function(error){
+            alert('an error occured');
+          }
+
+          pushNotification.register(
+      successHandler,
+      errorHandler,
+      {
+        'badge': 'true',
+        'sound': 'true',
+        'alert': 'true',
+        'forceShow' : 'true',
+        'ecb': 'onNotification',
+        'senderID': '1072546120442',
+      }
+      );
+
+        });
+})
+
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    var push = new Ionic.Push({
+      "debug": true
+    });
+
+    push.register(function(token) {
+      //alert(token._token);
+    });
+
+    // kick off the platform web client
+    Ionic.io();
+
+// this will give you a fresh user or the previously saved 'current user'
+var user = Ionic.User.current();
+
+// if the user doesn't have an id, you'll need to give it one.
+if (!user.id) {
+  user.id = Ionic.User.anonymousId();
+  // user.id = 'your-custom-user-id';
+}
+
+//persist the user
+user.save();
+
+});
+})
+
+
+.config(function($stateProvider, $urlRouterProvider,$httpProvider) {
+
+  $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+  $httpProvider.defaults.transformRequest = function(data){
+    if (data === undefined) {
+      return data;
+    }
+    return $.param(data);
+  }
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+  // setup an abstract state for the tabs directive
+  .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
+  })
+
+  // Each tab has its own nav history stack:
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.chats', {
+    url: '/chats',
+    views: {
+      'tab-chats': {
+        templateUrl: 'templates/tab-chats.html',
+        controller: 'ChatsCtrl'
+      }
+    }
+  })
+  .state('tab.chat-detail', {
+    url: '/chats/:chatId',
+    views: {
+      'tab-chats': {
+        templateUrl: 'templates/chat-detail.html',
+        controller: 'ChatDetailCtrl'
+      }
+    }
+  })
+
+  .state('tab.siren', {
+    url: '/siren',
+    views: {
+      'tab-siren': {
+        templateUrl: 'templates/tab-siren.html',
+        controller: 'SirenCtrl'
+      }
+    }
+  })
+  .state('tab.listmenu', {
+    url: '/listmenu',
+    views: {
+      'tab-listmenu': {
+        templateUrl: 'templates/list-menu.html',
+        controller: 'ListmenuCtrl'
+      }
+    }
+  })
+  .state('tab.account', {
+    url: '/account/:chatId',
+    views: {
+      'tab-listmenu': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
+  })
+
+  .state('tab.calendar', {
+    url: '/calendar',
+    views: {
+      'tab-listmenu': {
+        templateUrl: 'templates/tab-calendar.html',
+        controller: 'CalendarCtrl'
+      }
+    }
+  })
+
+  .state('tab.addfriend', {
+    url: '/addfriend',
+    views: {
+      'tab-listmenu': {
+        templateUrl: 'templates/tab-addfriend.html',
+        controller: 'AddfriendCtrl'
+      }
+    }
+  })
+
+  .state('tab.editaccount', {
+    url: '/editaccount',
+    views: {
+      'tab-listmenu': {
+        templateUrl: 'templates/tab-editaccount.html',
+        controller: 'EditaccountCtrl'
+      }
+    }
+  })
+
+  .state('tab.addcall', {
+    url: '/addcall',
+    views: {
+      'tab-listmenu': {
+        templateUrl: 'templates/tab-addcall.html',
+        controller: 'AddcallCtrl'
+      }
+    }
+  })
+
+  .state('tab.emergencycall', {
+    url: '/emergencycall',
+    views: {
+      'tab-listmenu': {
+        templateUrl: 'templates/tab-emergencycall.html',
+        controller: 'EmergencycallCtrl'
+      }
+    }
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/tab-member.html',
+    controller: 'MemberCtrl'
+  })
+
+
+  .state('register', {
+    url: '/register',
+    templateUrl: 'templates/tab-register.html',
+    controller: 'RegisterCtrl'
+  });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/login');
+
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,503 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope, Friend,$http, $localstorage, $ionicPopup) {
+ $scope.selection = [];
+ $scope.comment;
+
+ Friend.getfriend().success(function(data){
+  console.log(data.data)
+  $scope.friends = data.data;
+  console.log($scope.friends[0].username)
+});
+
+   // toggle selection for a given employee by name
+   $scope.toggleSelection = function toggleSelection(employeeName) {
+    var idx = $scope.selection.indexOf(employeeName);
+
+    if (idx > -1) {
+      $scope.selection.splice(idx, 1);
+    } else {
+      $scope.selection.push(employeeName);
+    }
+  };
+
+  $scope.sendcomment = function () {
+    $http({
+      url: "http://lab.kusumotolab.com/HelperSenior/index.php/useraccount/notify",
+      method: "POST", 
+      data: {target_username : $scope.selection, message : $scope.comment, username: JSON.parse($localstorage.get('login')).username }
+    }).success(function(response) {
+      console.log(response)
+      if (response.notify == 'fail') {
+       var alertPopup = $ionicPopup.alert({
+        title: 'พบข้อผิดพลาด!',
+        template: 'กรุณาลองใหม่อีกครั้ง'
+      });
+     } else{
+      var alertPopup = $ionicPopup.alert({
+        title: 'เรียบร้อย!',
+        template: 'ส่งข้อความสำเร็จ'
+      });
+      $scope.selection = [];
+      $scope.comment = '';
+      Friend.getfriend().success(function(data){
+        console.log(data.data)
+        $scope.friends = data.data;
+        console.log($scope.friends[0].username)
+      });
+
+    }
+
+  }).error(function(response) {
+    var alertPopup = $ionicPopup.alert({
+      title: 'พบข้อผิดพลาด!',
+      template: 'ไม่สามารถติดต่อเซิร์ฟเวอร์ได้'
+    });
+  });
+}
+
+})
+
+.controller('MemberCtrl', function ($scope,$http,$rootScope,$state, $ionicPopup,$localstorage) {
+  // if($localstorage.get('login')){
+  //   $scope.login = {
+  //     username : ,
+  //     password : JSON.parse($localstorage.get('login')).password
+  //   }
+  // } else {
+    //  $scope.login = {
+    //   username : '',
+    //   password : ''
+    // }
+  // }
+
+  
+  // $localstorage.set('name', 'Max');
+  // console.log($localstorage.get('name'));
+
+
+  // var post = $localstorage.getObject('post');
+  // console.log(post);
+
+
+  $scope.autologin = function() {
+    if($localstorage.get('login')) {
+     $http({
+      url: "http://lab.kusumotolab.com/HelperSenior/index.php/useraccount/login",
+      method: "POST", 
+      data: {username: JSON.parse($localstorage.get('login')).username, password: JSON.parse($localstorage.get('login')).password, device_token: JSON.parse($localstorage.get('push')).token}
+    }).success(function(response) {
+      if (response.login == 'success') {
+
+        $state.go('tab.siren')
+      // Ionic.Auth.login(authProvider, authSettings, loginDetails)
+      // .then(authSuccess, authFailure);
+
+    } else {
+      var alertPopup = $ionicPopup.alert({
+        title: 'พบข้อผิดพลาด!',
+        template: 'ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง'
+      });
+    }
+  }).error(function(response) {
+    var alertPopup = $ionicPopup.alert({
+      title: 'พบข้อผิดพลาด!',
+      template: 'ไม่สามารถติดต่อเซิร์ฟเวอร์ได้'
+    });
+  });
+} 
+}
+
+
+$scope.login = function() {
+  $http({
+    url: "http://lab.kusumotolab.com/HelperSenior/index.php/useraccount/login",
+    method: "POST", 
+    data: {username: $scope.login.username, password: $scope.login.password, device_token: JSON.parse($localstorage.get('push')).token}
+  }).success(function(response) {
+
+    if (response.login == 'success') {
+
+      $localstorage.setObject('login', {
+        username: $scope.login.username,
+        password: $scope.login.password,
+        email:  response.email
+      });
+
+      $state.go('tab.siren')
+        // Ionic.Auth.login(authProvider, authSettings, loginDetails).then(authSuccess, authFailure);
+
+      } else {
+        var alertPopup = $ionicPopup.alert({
+          title: 'พบข้อผิดพลาด!',
+          template: 'ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง'
+        });
+      }
+    }).error(function(response) {
+      var alertPopup = $ionicPopup.alert({
+        title: 'พบข้อผิดพลาด!',
+        template: 'ไม่สามารถติดต่อเซิร์ฟเวอร์ได้'
+      });
+    }); 
+  }
+})
+
+.controller('ListmenuCtrl', function($scope,$localstorage, $state) {
+
+  $scope.logout = function() {
+   $localstorage.clear();
+   $state.go('login')
+ }
+})
+
+
+
+.controller('AccountCtrl', function($scope,$stateParams, Account, $http,$localstorage,$state, $ionicPopup, Friend, $timeout) {
+
+
+  $scope.account;
+  $scope.friends;
+  $scope.$on('$ionicView.enter', function() {
+
+    Account.getaccount().success(function(data){
+      console.log(data.data)
+      $scope.account = data.data;
+      console.log($scope.account.username)
+    });
+
+    $timeout(function() {
+      Friend.getfriend().success(function(data){
+        console.log(data.data)
+        $scope.friends = data.data;
+        console.log($scope.friends[0].username)
+      });
+    }, 3000);
+
+
+
+  });
+
+
+  // $scope.getfriend = Chats.get($stateParams.chatId);
+  // console.log(Chats.get($stateParams.chatId));
+  // $scope.chats = Chats.all();
+  // $scope.chats.remove(0);
+
+})
+
+
+.controller('CalendarCtrl', function($scope) {
+ 'use strict';
+
+ $scope.calendar = {};
+ $scope.viewTitle = ""
+ $scope.changeMode = function (mode) {
+  $scope.calendar.mode = mode;
+};
+$scope.onViewTitleChanged = function (title) {
+  $scope.viewTitle = title;
+};
+
+$scope.today = function () {
+  $scope.calendar.currentDate = new Date();
+};
+})
+
+
+.controller('RegisterCtrl', function($scope,$http,$rootScope,$state, $ionicPopup,$ionicActionSheet) {
+
+ $scope.register = {
+  username : '',
+  firstname : '',
+  lastname : '',
+  email : '',
+  disease :'',
+  drug_allergy : '',
+  treatment : '' ,
+  personaldoctor : '',
+  hospital : '',
+  password : '',
+  lastPhoto : ''
+}
+
+$scope.select_pic = function() {
+
+   // Show the action sheet
+
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+     { text: 'ถ่ายรูปจากกล้อง' },
+     { text: 'อัพโหลดรูปภาพ' }
+     ],
+     titleText: 'อัพโหลดรูปโปรไฟล์',
+     cancelText: '<font color="red">ยกเลิก</font>',
+     cancel: function() {
+          // add cancel code..
+        },
+        buttonClicked: function(index) {
+          console.log(index)
+          if (index === 0) {
+            var options = {
+              quality: 50,
+              destinationType:0
+            };
+          } else {
+           var options = {
+            quality: 50,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            destinationType:0
+          };
+
+        }
+
+        navigator.camera.getPicture(onSuccess, onFail, options); 
+        //destination type was a base64 encoding
+        function onSuccess(imageData) {
+        //preview image on img tag
+        $('#image-preview').attr('src', "data:image/jpeg;base64,"+imageData);
+        //setting scope.lastPhoto 
+        $scope.register.lastPhoto = dataURItoBlob("data:image/jpeg;base64,"+imageData);
+      }
+      function onFail(message) {
+        // alert('Failed because: ' + message);
+      } 
+      return true;  
+    }
+  });
+ };
+
+ $scope.register = function() {
+  $http({
+    url: "http://lab.kusumotolab.com/HelperSenior/index.php/useraccount/register",
+    method: "POST", 
+    data: {username: $scope.register.username,firstname: $scope.register.firstname,lastname: $scope.register.lastname,
+      email: $scope.register.email,disease: $scope.register.disease,drug_allergy: $scope.register.drug_allergy,
+      treatment: $scope.register.treatment, personaldoctor: $scope.register.personaldoctor, hospital: $scope.register.hospital, password: $scope.register.password ,images: $('#image-preview').attr('src')}
+    }).success(function(response) {
+      if (response.register == 'success') {
+
+       var users = {
+        'email':  $scope.register.email,
+        'password': $scope.register.password
+      }
+
+      // Ionic.Auth.signup(users).then(signupSuccess, signupFailure);
+      $state.go('login')
+    } else {
+     var alertPopup = $ionicPopup.alert({
+      title: 'พบข้อผิดพลาด!',
+      template: response.messagefail
+    });
+   }
+ }).error(function(response) {
+   var alertPopup = $ionicPopup.alert({
+    title: 'พบข้อผิดพลาด!',
+    template: 'ไม่สามารถติดต่อเซิร์ฟเวอร์ได้'
+  });
+ }); 
+}
+})
+
+.controller('EmergencycallCtrl', function($scope,CustomCall) {
+  $scope.$on('$ionicView.enter', function() {
+    CustomCall.getcall().success(function(response) {
+      $scope.calldatas = response.data;
+    });
+  });
+
+})
+
+
+.controller('AddcallCtrl', function($scope,CustomCall,$state, $ionicPopup) {
+
+  $scope.call = {
+    'name' : '',
+    'tel' : ''
+  }
+  $scope.doAddCall = function () {
+    CustomCall.savecall($scope.call.name,$scope.call.tel).success(function(response) {
+      if (response.addcall == 'success') {
+
+        $state.go('tab.emergencycall', {}, {reload: true})
+      } else {
+       var alertPopup = $ionicPopup.alert({
+        title: 'พบข้อผิดพลาด!',
+        template: response.messagefail
+      });
+     }
+   }).error(function(response) {
+    var alertPopup = $ionicPopup.alert({
+      title: 'พบข้อผิดพลาด!',
+      template: 'ไม่สามารถติดต่อเซิร์ฟเวอร์ได้'
+    });
+  });
+
+ }
+})
+.controller('EditaccountCtrl', function($scope,$http,$rootScope,$state, $ionicPopup,$ionicActionSheet, Account) {
+
+  $scope.editaccount;
+  $scope.$on('$ionicView.enter', function() {
+    Account.getaccount().success(function(data){
+      console.log(data.data)
+      $scope.editaccount = data.data;
+      $('#image-preview').attr('src', $scope.editaccount.images);
+      console.log($scope.editaccount.username)
+    });
+  });
+
+  $scope.select_pic = function() {
+
+   // Show the action sheet
+
+   var hideSheet = $ionicActionSheet.show({
+     buttons: [
+     { text: 'ถ่ายรูปจากกล้อง' },
+     { text: 'อัพโหลดรูปภาพ' }
+     ],
+     titleText: 'อัพโหลดรูปโปรไฟล์',
+     cancelText: '<font color="red">ยกเลิก</font>',
+     cancel: function() {
+          // add cancel code..
+        },
+        buttonClicked: function(index) {
+          console.log(index)
+          if (index === 0) {
+            var options = {
+              quality: 50,
+              destinationType:0
+            };
+          } else {
+           var options = {
+            quality: 50,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+            destinationType:0
+          };
+
+        }
+
+        navigator.camera.getPicture(onSuccess, onFail, options); 
+        //destination type was a base64 encoding
+        function onSuccess(imageData) {
+        //preview image on img tag
+        $('#image-preview').attr('src', "data:image/jpeg;base64,"+imageData);
+        //setting scope.lastPhoto 
+        $scope.editaccount.lastPhoto = dataURItoBlob("data:image/jpeg;base64,"+imageData);
+      }
+      function onFail(message) {
+        // alert('Failed because: ' + message);
+      } 
+      return true;  
+    }
+  });
+ };
+
+ $scope.editaccount_submit = function() {
+  $http({
+    url: "http://lab.kusumotolab.com/HelperSenior/index.php/useraccount/editaccount",
+    method: "POST", 
+    data: {username: $scope.editaccount.username, firstname: $scope.editaccount.firstname,lastname: $scope.editaccount.lastname,
+      email: $scope.editaccount.email,disease: $scope.editaccount.disease,drug_allergy: $scope.editaccount.drug_allergy,
+      treatment: $scope.editaccount.treatment, personaldoctor: $scope.editaccount.personaldoctor, hospital: $scope.editaccount.hospital, password: $scope.editaccount.password ,images: $('#image-preview').attr('src')}
+    }).success(function(response) {
+      if (response.editaccount == 'success') {
+       $state.go('login')
+     } else {
+       var alertPopup = $ionicPopup.alert({
+        title: 'พบข้อผิดพลาด!',
+        template: response.messagefail
+      });
+     }
+   }).error(function(response) {
+     var alertPopup = $ionicPopup.alert({
+      title: 'พบข้อผิดพลาด!',
+      template: 'ไม่สามารถติดต่อเซิร์ฟเวอร์ได้'
+    });
+   }); 
+ }
+})
+
+
+.controller('AddfriendCtrl', function($scope,$http,$rootScope,$state, $ionicPopup,$localstorage) {
+  $scope.addfriend = {
+    username : ''
+  }
+  $scope.doAddfriend = function () {
+    $http({
+      url: "http://lab.kusumotolab.com/HelperSenior/index.php/useraccount/addfriend",
+      method: "POST", 
+      data: {username: JSON.parse($localstorage.get('login')).username, usernamefriend: $scope.addfriend.username}
+    }).success(function(response) {
+      if (response.addfriend == 'success') {
+       $state.go('tab.siren')
+     } else {
+      var alertPopup = $ionicPopup.alert({
+        title: 'พบข้อผิดพลาด!',
+        template: 'ไม่พบชื่อผู้ใช้งานในระบบ'
+      });
+    }
+  }).error(function(response) {
+    var alertPopup = $ionicPopup.alert({
+      title: 'พบข้อผิดพลาด!',
+      template: 'ไม่สามารถติดต่อเซิร์ฟเวอร์ได้'
+    });
+  }); 
+};
+})
+
+
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats =  Chats.all();
+
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('SirenCtrl', function($scope,$http,$localstorage,$state, $ionicPopup) {
+
+  $scope.sendnotify = function() {
+    $http({
+      url: "http://lab.kusumotolab.com/HelperSenior/index.php/useraccount/sendhelpall",
+      method: "POST", 
+      data: {username: JSON.parse($localstorage.get('login')).username}
+    }).success(function(response) {
+
+      if (response.sendhelpall == 'success') {
+       var alertPopup = $ionicPopup.alert({
+        title: 'เรียบร้อย!',
+        template: 'แจ้งเตือนเรียบร้อย'
+      });
+
+     } else {
+      var alertPopup = $ionicPopup.alert({
+        title: 'พบข้อผิดพลาด!',
+        template: 'มีข้อผิดพลาดระหว่างส่งข้อมูล'
+      });
+    }
+  }).error(function(response) {
+    var alertPopup = $ionicPopup.alert({
+      title: 'พบข้อผิดพลาด!',
+      template: 'ไม่สามารถติดต่อเซิร์ฟเวอร์ได้'
+    });
+  }); 
+}
+
+$scope.settings = {
+  enableFriends: true
+};
+
+});
