deddf661e29ce66630fa0425785ebabba8f03e4d
Arinee Ayuya <Arinee Ayuya>
2017-01-11 21:28:25 +0700

    add new version.

---
+++ run-scanner.sh
@@ -0,0 +1,14 @@
+wget https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/sonar-scanner-2.8.zip -O ~/scanner.zip
+unzip ~/scanner.zip -d ~
+
+rm -rf node_modules
+
+cat << EOF > sonar-project.properties
+  sonar.host.url=${SONAR_HOST_URL}
+  sonar.projectKey=${SONAR_PROJECT_KEY}
+  sonar.projectName=${SONAR_PROJECT_NAME}
+  sonar.projectVersion=${SONAR_PROJECT_VERSION}
+  sonar.sources=./www
+EOF
+
+ ~/sonar-scanner-2.8/bin/sonar-scanner
